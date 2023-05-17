a8da55cb6a6d835f9e6885013b2a286657f973e9
Rodrigo Maluf <rodrigomaluf@gmail.com>
2023-05-17 08:05:14 -0300

    commit inicial

---
+++ src/main/java/br/com/estudos/App.java
@@ -0,0 +1,13 @@
+package br.com.estudos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
